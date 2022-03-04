e67a4cbd792839cc60a78190da3fcb57fc81d32e
Stacy Smith <stacy@kit10.us>
2022-03-04 10:05:34 -0600

    Updates, for backup.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using System.Windows;
+
+using Windows.Storage.Pickers;
+using Windows.UI.Popups;
+using Windows.Storage;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SilentSheet
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void File_Open_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
+            openPicker.FileTypeFilter.Add(".csv");
+
+            StorageFile storageFiles = await openPicker.PickSingleFileAsync();
+            if( storageFiles != null )
+            {
+
+                var dialog = new MessageDialog("test");
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
